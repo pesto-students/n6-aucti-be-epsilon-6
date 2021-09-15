df922883eab1a71fbdcb64ad6ccfb15fda590dd8
Kirushan <kirushanbalakrishnan@gmail.com>
2021-09-15 17:18:39 +0530

    after merge

--- aucti-web/src/components/Tests/Shared/ProductCardSkeleton.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import ProductCardSkeleton from "../../Shared/ProductCardSkelton";
-
-describe("render product card skeleton", () => {
-  const { container } = render(<ProductCardSkeleton />);
-  test("non empty div rendered", () => {
-    expect(container.firstChild).not.toBeEmpty;
-  });
-});
